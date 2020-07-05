504798cc49aa0e8b7c4935ff82a417c5f36c8ce3
bowu9458 <67699284+bowu9458@users.noreply.github.com>
2020-07-05 11:53:34 -0400

    new branch
    My version has a conflict with Elise version. So I created this new branch

--- qi.php
+++ qi.php
@@ -14,62 +14,57 @@
 
 <body>
 	<?php include "inc/nav.php"; ?>
-	<header>
 
+	<header>
 		<h1>Qi Miao</h1>
 		<div>Major in Computer Science</div>
 		<div>Major in Digital Media Studies</div>
-
-
 	</header>
 
-	<div class="content">
 
-	<article>
-		<h2>Computer Science</h2>
-		<p>I am pursuing a BA in Computer Science and want to focus on the HCI(Human Computer Interaction) track. I am interested in the realtionship between high-tech and people, so I want to use technologies to make our life a little bit easier if possible.</p>
+	<div class="background">
+		<div class="container">
+		<article>
+			<h2>Computer Science</h2>
+			<p>I am pursuing a BA in Computer Science and want to focus on the HCI(Human Computer Interaction) track. I am interested in the realtionship between high-tech and people, so I want to use technologies to make our life a little bit easier if possible.</p>
 
-		<h2>Digital Media Studies</h2>
-		<p>I am also pursuing a degree in Digital Media Studies. The reasons are similar to those for my another degree, BA in Computer Science. Besides, DMS program can offer me some opportunities in practicing and desiging products.</p>
+			<h2>Digital Media Studies</h2>
+			<p>I am also pursuing a degree in Digital Media Studies. The reasons are similar to those for my another degree, BA in Computer Science. Besides, DMS program can offer me some opportunities in practicing and desiging products.</p>
 
-		<h2>Music</h2>
-		<p>I love listening to the music, playing instruments and singsing. I like Cocteau Twins and Sophie Zelmani. I played string instruments before,and I started to learn guitar and western music since I entered college. I am not so professional in it, but I will keep practicing and embracing suggestions from other guitar players. Singing is kinda a way for me to "waste" time. I almost sing everyday and joined the concert choir in U of R.</p>
+			<h2>Music</h2>
+			<p>I love listening to the music, playing instruments and singsing. I like Cocteau Twins and Sophie Zelmani. I played string instruments before,and I started to learn guitar and western music since I entered college. I am not so professional in it, but I will keep practicing and embracing suggestions from other guitar players. Singing is kinda a way for me to "waste" time. I almost sing everyday and joined the concert choir in U of R.</p>
 
-		<h2>Languages</h2>
-		<p>I am interested in learning different languages. I speak Mandarin, English, a little bit Japanese. I am also learning German and Cantonese myself. I found different grammar structures interesting and will explore as much as possible.</p>
+			<h2>Languages</h2>
+			<p>I am interested in learning different languages. I speak Mandarin, English, a little bit Japanese. I am also learning German and Cantonese myself. I found different grammar structures interesting and will explore as much as possible.</p>
 
-		<h2>Astrology</h2>
-		<p>I like observing and talking to different people, so I started learning psychology in my high school. Then, I got to know a psychologist, Stephen Arroyo, who is also a astrologist. He wrote some books, indicating the relationships between psychology and astrology, and those contents intrigued me a lot.</p>
+			<h2>Astrology</h2>
+			<p>I like observing and talking to different people, so I started learning psychology in my high school. Then, I got to know a psychologist, Stephen Arroyo, who is also a astrologist. He wrote some books, indicating the relationships between psychology and astrology, and those contents intrigued me a lot.</p>
 
-		<h2>Active Minds</h2>
-		<p>Active Minds is a student organization at University of Rochester, focusing on mental health of students on campus. We regularly organize activities such as lectures and exhibitions in order to appeal to all students on campus to pay attention to and face up to their mental health problems. I am the Business Manager of this organization, and is responsible for things like ordering food and materials for our activities like GIM and GMM and recording the money transaction needed for the events.</p>
+			<h2>Active Minds</h2>
+			<p>Active Minds is a student organization at University of Rochester, focusing on mental health of students on campus. We regularly organize activities such as lectures and exhibitions in order to appeal to all students on campus to pay attention to and face up to their mental health problems. I am the Business Manager of this organization, and is responsible for things like ordering food and materials for our activities like GIM and GMM and recording the money transaction needed for the events.</p>
 
-		<h2>Modern Language and Culture Undergraduate Council</h2>
-		<p>MLC Undergraduate Council is supported by the language center, offering a platform for students around the campus to embrace different languages and cultures. We will hold activities on different holidays and celebrate in different ways under various cultural backgrounds. I am the VP(Vice President) of this organization.</p>
+			<h2>Modern Language and Culture Undergraduate Council</h2>
+			<p>MLC Undergraduate Council is supported by the language center, offering a platform for students around the campus to embrace different languages and cultures. We will hold activities on different holidays and celebrate in different ways under various cultural backgrounds. I am the VP(Vice President) of this organization.</p>
+		</article>
 
+		<aside>
+			<h2>Courses Taken</h2>
 
-	</article>
+			<ul>
+				<li>CSC 170</li>
+				<li>CSC 171</li>
+				<li>CSC 172</li>
+				<li>CSC 173</li>
+				<li>DMS 101</li>
+				<li>DMS 104</li>
+			</ul>
+		</aside>
 
-	<aside>
-		
-		<h2>Courses Taken</h2>
-
-		<ul>
-			<li>CSC 170</li>
-			<li>CSC 171</li>
-			<li>CSC 172</li>
-			<li>CSC 173</li>
-			<li>DMS 101</li>
-			<li>DMS 104</li>
-		</ul>
-
-	</aside>
-
-	<footer>
-		<p>CSC 174: Advanced Front-end Web - Project 1</p>
-	</footer>
-
-
+		<footer>
+			<p>CSC 174: Advanced Front-end Web - Project 1</p>
+		</footer>
+		</div> <!-- center container -->
+	<div><!--  background container -->
 </body>
-</div>
+
 </html>
