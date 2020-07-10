572317f0af60b96b090dda8bc8447a71a8437267
Bonnie Yang <bonniehyang@gmail.com>
2020-07-10 12:05:08 -0400

    updated navigation

--- inc/nav.php
+++ inc/nav.php
@@ -1,8 +1,10 @@
 <nav class="menu">
 		<ul>
 			<li><a href="index.php">Home</a></li>
+      <li><a href="hy.php">Huiyu (Bonnie) Yang</a></li>
+      <li><a href="joe.php">Joe Madejski</a></li>
 			<li><a href="qi.php">Qi Miao</a></li>
-			<li><a href="elise.php">Elise Frelinger</a></li>
-			<li><a href="bo.php">Bo Wu</a></li>
+
+
 		</ul>
 </nav>
