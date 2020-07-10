9739f509c0cafda1981c847a9a8338d224d0112e
Bonnie Yang <bonniehyang@gmail.com>
2020-07-10 12:01:36 -0400

    added files from joe's project 1

---
+++ joe.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Joe Madejski</title>
+		 <link rel ="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/navigation.css">
+	</head>
+	<body class="container">
+	
+
+	<?php include "inc/nav.php" ?>
+	
+		<div>
+			<header>
+				<h1>About Joe Madejski</h1>
+			</header>
+
+			
+
+			<article class="content">
+				<h2>Background</h2>
+					<p>Joe Madejski was born in Western NY and has lived there since. He is the youngest of his siblings, and therefore the best of them. A graduate of Albion Central Schools, he is currently attending the University of Rochester and pursing a degree in Cell and Tissue Engineering with a cluster in abnormal psychology. The various computer courses he has taken are necessary to become aquainted with an omnipresent component of modern society. </p>
+			</article>
+
+			<aside class="side">
+				<figure>
+					<img src="images/main.jpg" alt="A dog" class="shak">
+					<figcaption>A dog. Not my dog mind you, but a dog. (Hover on me!)</figcaption>
+
+				</figure>
+			</aside>
+
+			<header>
+				<h1>Hobbies</h1>
+			</header>
+			<article class="side">
+				<h2>Hobbies</h2>
+				<div>These are mostly water based</div>
+				<ul>
+					<li>Swimming</li>
+					<li>Sailing</li>
+					<li>Fishing</li>
+					<li>And apparently CSC classes</li>
+				</ul>
+			</article>
+			<aside class="content">
+				<figure>
+					<img src="images/pool.jpg" alt="A pool" class="shak">
+					<figcaption>A pool. Not my pool mind you, but a pool. (Hover on me!)</figcaption>
+
+				</figure>
+			</aside>
+
+			<header>
+				<h1>The Future</h1>
+			</header>
+
+				<article class="content">
+					<h2>What Come Next?</h2>
+						<p>Who knows what the future will hold? Mayber I'll become a genetic engineer, or maybe I'll dive more into computer science. Either way, I'm always excited to seize the opportunities ahead of me and make more opportunities of my own!</p>
+				</article>
+
+				<aside class="side">
+					<figure>
+						<img src="images/q.jpg" alt="A question mark" class="shak">
+						<figcaption>What comes next? (Hover on me!)</figcaption>
+
+					</figure>
+				</aside>
+
+			<header>
+				<h1>Fun Facts</h1>
+			</header>
+			<article class="side">
+				<h2>Facts</h2>
+					<ul>
+						<li> Graduate of Albion Central Schools</li>
+						<li> Eagle Scout </li>
+						<li> Biomedical Engineering Undergrad</li>
+						<li> Specializing in Cell, Tissue, and Genetic Engineering</li>
+						<li> Competent in MATLAB</li>
+					</ul>
+			</article>
+			<aside class="content">
+				<figure>
+					<img src="images/q2.jpg" alt="Multiple question marks" class="shak">
+					<figcaption>What do you want to know? (Hover on me!)</figcaption>
+
+				</figure>
+			</aside>
+			
+			<footer> 
+				<p>CSC 174: Advanced Front-End Web Design and Development Project 1</p> 
+				<p>
+					<a href="http://csc174.org/lab01/jmadejsk/index.php">Check out my website</a>
+				</p>
+			</footer>
+
+		</div>
+	</body>
+</html>
