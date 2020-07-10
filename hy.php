35c3df4dbf432f1fa833358b5268431b440b5543
Bonnie Yang <bonniehyang@gmail.com>
2020-07-10 14:03:12 -0400

    removed 4 fencing pic, kept 1

--- hy.php
+++ hy.php
@@ -17,13 +17,9 @@
 		<p>Bonnie is a rising senior majoring in Brain and Cognitive Sciences at the University of Rochester, minoring in Clinical Psychology and taking a cluster in German. Until two months ago, she was also planning to minor in Computer Science, but that is no longer possible for scheduling reasons. She has been a member of U of R's Fencing club since freshman year, and took over the job of vice president last winter. Before she came to Rochester, she went to high school in Shanghai, China, where she was born and raised. </p>
 
     <h2>Fencing</h2>
-		<figure><img src="images/fencing.jpg" alt="fencing"></figure>
+    <figure class="fencing"><img src="images/fencingpic.jpg" alt="fencing"></figure>
 		<p>I wasn't planning on joining any clubs when I first came here, but I still went to the club fair because I was bored that afternoon. I walked past someone holding a sword, and I heard a voice in my head: "I should do that. " And that's how I, probably the least coordinated, least athletic person, ended up in a sports club. </p>
 		<p>I became the club's photographer the second year, mostly by accident. It was the first tournament after our last photographer had just graduated, and I happen to have my camera in the bag I brought with me. Here're some photos I've taken since then: </p>
-		<figure class="fencing"><img src="images/fencingpic1.jpg" alt="fencing"></figure>
-		<figure class="fencing"><img src="images/fencingpic2.jpg" alt="fencing"></figure>
-		<figure class="fencing"><img src="images/fencingpic3.jpg" alt="fencing"></figure>
-		<figure class="fencing"><img src="images/fencingpic4.jpg" alt="fencing"></figure>
 
 		<h2>Brain & Cognitive Sciences</h2>
 		<p>I was undecided in my major for a while, and I started to think about choosing BCS as my major because of its wide variety of courses. As a somewhat interdisciplinary major, I'll need to take classes related to linguistics, optics, computer science, as well as psychology and neuroscience. Like most people in my major, I started in the Perception and Action track. Eventurally, I decided to switch into the Neurobiology and Neuropsychology track in my junior year because I noticed the parts that attracted me the most were related to neurobiology and neuroanatomy. </p>
