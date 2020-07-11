c793d252eeeae870850ce6da5bb8e6d250cf159e
Iris Qi Miao <irism824-NF@users.noreply.github.com>
2020-07-11 18:35:27 +0800

    change the major part

--- qi.php
+++ qi.php
@@ -14,28 +14,25 @@
 		<div class="container">
 		<article>
 			<h2>Background</h2>
-			<figure><img src="images/pic.jpg" alt="Qi's photo"></figure>
+			<figure><img src="images/qi_photo.jpg" alt="Qi's photo"></figure>
 			<p>My name is Qi Miao. I also go by Iris. I am a rising junior, majoring in Computer Science and Digital Media Studies. I participate in Modern Language and Culture Undergraduate Council and Active Minds. I am interested in psychology, so I am going to minor in psychology as well. I know some programming languages like JAVA, HTML, CSS, and C, and I have some expereince in coding. I also like studying languages and astrology myself.</p>
 
-
+			<h2>Major</h2>
+			<h3>Computer Science & Digital Media Studies</h3>
 			<p>I am pursuing a BA in Computer Science and want to focus on the HCI(Human Computer Interaction) track. I am interested in the realtionship between high-tech and people, so I want to use technologies to make our life a little bit easier if possible.</p>
-
-			<h2>Digital Media Studies</h2>
 			<p>I am also pursuing a degree in Digital Media Studies. The reasons are similar to those for my another degree, BA in Computer Science. Besides, DMS program can offer me some opportunities in practicing and desiging products.</p>
 
 			<h2>Music</h2>
 			<p>I love listening to the music, playing instruments and singsing. I like Cocteau Twins and Sophie Zelmani. I played string instruments before,and I started to learn guitar and western music since I entered college. I am not so professional in it, but I will keep practicing and embracing suggestions from other guitar players. Singing is kinda a way for me to "waste" time. I almost sing everyday and joined the concert choir in U of R.</p>
 
-			
+		</article>
 
+		<aside>
 			<h2>Active Minds</h2>
 			<p>Active Minds is a student organization at University of Rochester, focusing on mental health of students on campus. We regularly organize activities such as lectures and exhibitions in order to appeal to all students on campus to pay attention to and face up to their mental health problems. I am the Business Manager of this organization, and is responsible for things like ordering food and materials for our activities like GIM and GMM and recording the money transaction needed for the events.</p>
 
 			<h2>Modern Language and Culture Undergraduate Council</h2>
 			<p>MLC Undergraduate Council is supported by the language center, offering a platform for students around the campus to embrace different languages and cultures. We will hold activities on different holidays and celebrate in different ways under various cultural backgrounds. I am the VP(Vice President) of this organization.</p>
-		</article>
-
-		<aside>
 			<h2>Courses Taken</h2>
 
 			<ul>
