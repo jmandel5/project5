8c2feb6309bb6e2452304830f68f6c4552821805
Joshua <hijoshuachoi@gmail.com>
2020-07-20 11:15:43 -0400

    F-pattern page

--- students.php
+++ students.php
@@ -11,28 +11,67 @@ include "inc/html-top.php";
 <div class="background home">
     <div class="container">
         <article>
-            <div class="column_1">
-                <h2>Huiyu (Bonnie) Yang</h2>
-                <a href="hy.php"><img src="images/hy_photo.jpg" alt="Bonnie's photo"></a>
-                <div class="major">Major in Brain and Cognitive Sciences</div>
-                <div class="major">Minor in Clinical Psychology</div>
-                <a href="http://csc174.org/lab01/hyang42/index.php">View Personal Website</a>
+
+
+            <div class="profile">
+                <div class="profile_img">
+                    <img src="images/hy_photo.jpg" alt="Bonnie's photo">
+                </div>
+                <div class="split">
+                    <div>
+                        <h2>Huiyu (Bonnie) Yang</h2>
+                        <h3 class="major">Major in Brain and Cognitive Sciences</h3>
+                        <h3 class="major">Minor in Clinical Psychology</h3>
+                        <p>My name is Bonnie Yang. I am a senior at the University of Rochester studying Neurobiology and Clinical
+                            Psychology, and taking a cluster in German. Until two months ago, I was also planning to minor in
+                            Computer Science, but that is no longer possible for scheduling reasons.</p>
+                    </div>
+                    <div class="link">
+                        <a href="http://csc174.org/lab01/hyang42/index.php" class="button-link">View Personal Website</a>
+                    </div>
+
+                </div>
             </div>
 
-            <div class="column_2">
-                <h2>Joe Madejski</h2>
-                <a href="jo3.php"><img src="images/joe_photo.jpg" alt="Joe's photo"></a>
-                <div class="major">Major in Cell and Tissue Engineering</div>
-                <div class="major">Cluster in Abnormal Psychology</div>
-                <a href="http://csc174.org/lab01/jmadejsk/">View Personal Website</a>
+            <div class="profile">
+                <div class="profile_img">
+                    <img src="images/joe_photo.jpg" alt="Joe's photo">
+                </div>
+                <div class="split">
+                    <div>
+                        <h2>Joe Madejski</h2>
+                        <h3 class="major">Major in Cell and Tissue Engineering</h3>
+                        <h3 class="major">Cluster in Abnormal Psychology</h3>
+                        <p>My name is Joe Madejski. I was born in Western NY and have lived there since. I am the youngest of his
+                            siblings, and therefore the best of them. A graduate of Albion Central Schools, I am currently attending
+                            the University of Rochester studying Cell and Tissue Engineering.</p>
+                    </div>
+                    <div class="link">
+                        <a href="http://csc174.org/lab01/jmadejsk/">View Personal Website</a>
+                    </div>
+                </div>
+
             </div>
 
-            <div class="column_3">
-                <h2>Qi Miao</h2>
-                <a href="qi.php"><img src="images/qi_photo.jpg" alt="Qi's photo"></a>
-                <div class="major">Major in Computer Science</div>
-                <div class="major">Major in Digital Media Studies</div>
-                <a href="http://csc174.org/lab01/qmiao3/index.php">View Personal Website</a>
+            <div class="profile">
+                <div class="profile_img">
+                    <img src="images/qi_photo.jpg" alt="Qi's photo">
+                </div>
+                <div class="split">
+                    <div>
+                        <h2>Qi Miao</h2>
+                        <h3 class="major">Major in Computer Science</h3>
+                        <h3 class="major">Major in Digital Media Studies</h3>
+                        <p>My name is Qi Miao. I also go by Iris. I am a rising junior, majoring in Computer Science and Digital
+                            Media Studies. I participate in Modern Language and Culture Undergraduate Council and Active Minds. I am
+                            interested in psychology, so I am going to minor in psychology as well. I know some programming
+                            languages like JAVA, HTML, CSS, and C, and I have some expereince in coding. I also like studying
+                            languages and astrology myself.</p>
+                    </div>
+                    <div class="link">
+                        <a href="http://csc174.org/lab01/qmiao3/index.php">View Personal Website</a>
+                    </div>
+                </div>
             </div>
         </article>
 
