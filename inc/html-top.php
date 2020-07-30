ea9810e274c9f81b1f603c07a09716014083f4ca
KarmaPanda <desmondlai30@gmail.com>
2020-07-30 14:46:18 -0400

    Reorganized order of custom css to act as override.css

--- inc/html-top.php
+++ inc/html-top.php
@@ -5,16 +5,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Project 4 - DC</title>
-    <?php
-    echo $customCSS;
-    if ($useNav) {
-        echo "<link rel=\"stylesheet\" href=\"css/navigation.css\">";
-    }?>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto%20Sans%20JP">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pangolin">
     <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <?php
+    echo $customCSS;
+    if ($useNav) {
+        echo "<link rel=\"stylesheet\" href=\"css/navigation.css\">";
+    }?>
 </head>
 <body class="<?php echo $bodyCSS ?>">
 <?php
