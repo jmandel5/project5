6ccb9645bf2c856daac2a3a72d03ca3a46cb8b15
Joshua <hijoshuachoi@gmail.com>
2020-08-01 22:21:07 -0400

    Bootstrap Forms and Navbar

--- inc/nav.php
+++ inc/nav.php
@@ -1,10 +1,19 @@
-<nav class="menu">
-    <h2><a href="index.php">CSC 174</a></h2>
-    <ul>
-        <li><a href="students.php">Home</a></li>
-        <li><a href="new.php">Add New Student</a></li>
-        <!--<li><a href="hy.php">Huiyu (Bonnie) Yang</a></li>
-        <li><a href="joe.php">Joe Madejski</a></li>
-        <li><a href="qi.php">Qi Miao</a></li>-->
-    </ul>
+<nav class=" menu navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="logo"><a href="index.php">CSC 174</a></div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-home nav-item">
+                <a class="nav-link" href="students.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="new.php">Add New Student</a>
+            </li>
+            <!--<li><a href="hy.php">Huiyu (Bonnie) Yang</a></li>
+            <li><a href="joe.php">Joe Madejski</a></li>
+            <li><a href="qi.php">Qi Miao</a></li>-->
+        </ul>
+    </div>
 </nav>
